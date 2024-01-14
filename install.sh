e1a0059f03fa988fe50d46bcf5935e1c0f7008fd
Zic Stardust <git@zicstardust.com>
2024-01-14 14:38:28 -0400

    feat: unintall

--- install.sh
+++ install.sh
@@ -16,6 +16,16 @@ ln -s ~/.local/opt/firefox-dev/browser/chrome/icons/default/default48.png ~/.loc
 ln -s ~/.local/opt/firefox-dev/browser/chrome/icons/default/default64.png ~/.local/share/icons/hicolor/64x64/apps/firefox-dev.png
 ln -s ~/.local/opt/firefox-dev/browser/chrome/icons/default/default128.png ~/.local/share/icons/hicolor/128x128/apps/firefox-dev.png
 
+# Create uninstall script
+cat > ~/.local/opt/firefox-dev/uninstall.sh <<UNINSTALL
+#!/bin/bash
+rm -Rf ${HOME}/.local/share/icons/hicolor/{16x16,32x32,48x48,64x64,128x128}/apps/firefox-dev.png
+rm -f ${HOME}/.local/bin/firefox-dev 
+rm -f ${HOME}/.local/share/applications/firefox-dev.desktop 
+rm -Rf ${HOME}/.local/opt/firefox-dev
+
+UNINSTALL
+chmod +x ~/.local/opt/firefox-dev/uninstall.sh
 
 # Create desktop launcher
 cat > ~/.local/share/applications/firefox-dev.desktop <<DESKTOP_ENTRY
@@ -30,7 +40,7 @@ MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xm
 StartupNotify=true
 Categories=Network;WebBrowser;
 Keywords=web;browser;internet;
-Actions=new-window;new-private-window;profile-manager;
+Actions=new-window;new-private-window;profile-manager;uninstall;
 StartupWMClass=Firefox Developer Edition
 
 [Desktop Action new-window]
@@ -44,4 +54,8 @@ Exec=${HOME}/.local/bin/firefox-dev --private-window %u
 [Desktop Action profile-manager]
 Name=Open Profile Manager
 Exec=${HOME}/.local/bin/firefox-dev -p %u
+
+[Desktop Action uninstall]
+Name=Uninstall
+Exec=${HOME}/.local/opt/firefox-dev/uninstall.sh
 DESKTOP_ENTRY
